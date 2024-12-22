2c415a35da09fec163f69f6eb39df46cd47bf5f6
ilya505 <niv1203@mail.ru>
2024-12-22 13:52:27 +0600

    добавление возможности писать знаки

--- main.py
+++ main.py
@@ -10,16 +10,25 @@ def print_umn(pr1, pr2):
 def print_min(pr1, pr2):
     print(int(pr1) - int(pr2))
 
+
 def print_plus(pr1, pr2):
     print(int(pr1) + int(pr2))
 
 
 b = input()
+a = input()
 c = input()
-print_del(b,c)
-print_umn(b,c)
-print_min(b,c)
-print_plus(b,c)
+if a=='/':
+    print_del(b,c)
+elif a=='*':
+    print_umn(b,c)
+elif a=='-':
+    print_min(b,c)
+elif a=='+':
+    print_plus(b,c)
+else:
+    print("не верный символ")
+
 
 
 
