9c0ae05a434d7d23916d1a98566432a12b6f3ff6
ilya505 <niv1203@mail.ru>
2024-12-22 13:40:24 +0600

    калькулятор с помощью функций выдающий знаки +,-,*,/

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_del(pr1, pr2):
+    print(int(int(pr1) / int(pr2)))
 
 
-# Press the green button in the gutter to run the script.
-print_hi('PyCharm ')
+def print_umn(pr1, pr2):
+    print(int(pr1) * int(pr2))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def print_min(pr1, pr2):
+    print(int(pr1) - int(pr2))
+
+def print_plus(pr1, pr2):
+    print(int(pr1) + int(pr2))
+
+
+b = input()
+c = input()
+print_del(b,c)
+print_umn(b,c)
+print_min(b,c)
+print_plus(b,c)
+
+
+
+
+
+
+
+
+
+
